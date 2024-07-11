843380a06315988e60fdddbf8d047c6fd577e713
Arnab Chatterjee <arnabchat2001@gmail.com>
2024-07-12 01:23:21 +0530

    LLM initialized , and packages configured

---
+++ src/lib/pinecone-client.ts
@@ -0,0 +1,28 @@
+import { Pinecone } from '@pinecone-database/pinecone';
+import { env } from "./config";
+
+let pineconeClientInstance: Pinecone | null = null;
+
+// Initialize Pinecone client and connect to the index.
+async function initPineconeClient() {
+  try {
+    const pineconeClient = new Pinecone({
+      apiKey: env.PINECONE_API_KEY,
+    });
+
+    return pineconeClient;
+  } catch (error) {
+    console.error("Error initializing Pinecone client:", error);
+    throw new Error("Failed to initialize Pinecone Client");
+  }
+}
+
+export async function getPineconeClient() {
+  if (!pineconeClientInstance) {
+    pineconeClientInstance = await initPineconeClient();
+  }
+
+  return pineconeClientInstance;
+}
+
+// Example usage to ensure the client is working
